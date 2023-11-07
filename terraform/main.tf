85bceb0639132e398ec9374db003f867b38e652e
zw-dev-01 <148320059+zw-dev-01@users.noreply.github.com>
2023-11-07 14:47:06 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,13 +61,13 @@ resource "proxmox_vm_qemu" "prox-vm" {
   }
 
   provisioner "local-exec" {
-      working_dir = "ansible/"
-      command = "ansible-playbook -u ${var.user} --key-file ../${var.ssh_keys["priv"]} -i ${var.ips[count.index]}, provision.yaml"
+      working_dir = "../ansible/"
+      command = "ansible-playbook -u ${var.user} --key-file ../terraform/${var.ssh_keys["priv"]} -i ${var.ips[count.index]}, provision.yaml"
   }
   
   provisioner "local-exec" {
-      working_dir = "ansible/"
-      command = "ansible-playbook -u ${var.user} --key-file ../${var.ssh_keys["priv"]} -i ${var.ips[count.index]}, install-qemu-guest-agent.yaml"
+      working_dir = "../ansible/"
+      command = "ansible-playbook -u ${var.user} --key-file ../terraform/${var.ssh_keys["priv"]} -i ${var.ips[count.index]}, install-qemu-guest-agent.yaml"
   }
 
 }
